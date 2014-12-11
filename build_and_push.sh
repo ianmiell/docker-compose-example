fd6b2d47bc6265929e95a6f0b4354cef6cabf1d1
Ian Miell <ian.miell@gmail.com>
2014-12-11 08:21:52 +0000

    more user-friendly

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "Logging in to docker hub"
 docker login
+echo "Building sqlite image"
 docker build --no-cache -t imiell/sqlite .
+echo "Pushing sqlite image"
 docker push imiell/sqlite
--- setup_dbs.sh
+++ setup_dbs.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+echo "Creating directory"
 mkdir -p /tmp/sqlitedbs
 cd /tmp/sqlitedbs
+echo "Removing old DBs"
 rm -f test live
+echo "Creating DBs"
 echo 'create table t1(c1 text);' | sqlite test
-echo 'insert into t1 values ("test");' | sqlite test
 echo 'create table t1(c1 text);' | sqlite live
+echo "Inserting data"
+echo 'insert into t1 values ("test");' | sqlite test
 echo 'insert into t1 values ("live");' | sqlite live
-cd -
-
+cd - > /dev/null 2>&1 
+echo "All done OK"
